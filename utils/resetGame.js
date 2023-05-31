99811e78246034b9a3356528e2c027b8cd0a07cb
lukedxvxes <lukedaviesweb+1@gmail.com>
2023-05-31 20:04:21 +0700

    updates old game

---
+++ utils/resetGame.js
@@ -0,0 +1,13 @@
+import { setupGame } from './setupGame';
+import { updateGame } from './updateGame';
+
+export const resetGame = () => {
+  document.querySelectorAll('.tile').forEach((tile) => {
+    tile.remove();
+  });
+  setupGame();
+
+  document.querySelectorAll('.tile').forEach((tile) => {
+    tile.addEventListener('keyup', (e) => updateGame(e));
+  });
+};
